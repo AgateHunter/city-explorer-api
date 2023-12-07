7d905981628319f918c5af08284406a2c3aa52ac
NATALIE STONE <NATALIE.I.STONE@OUTLOOK.COM>
2023-12-07 14:08:46 -0800

    updated full server functionality

--- server.js
+++ server.js
@@ -3,6 +3,44 @@ const cors = require('cors');
 require('dotenv').config();
 const weatherData = require('./data/weather.json');
 
+// create a Forecast class
+class Forecast{
+  constructor(lat,lon,searchQuery){
+    this.lat = lat;
+    this.lon = lon;
+    this.searchQuery = searchQuery;
+  }
+
+  // create a method to get the data
+  getWeather(){
+    const searchQueryToLower = this.searchQuery.toLowerCase();
+    const city = this.dataObject.find(city =>{
+      return (
+        city.city_name.toLowerCase() === searchQueryToLower
+      );
+    });
+
+    const data = {
+      city_name: city.city_name,
+      lat: city.lat,
+      lon: city.lon,
+      country_code: city.country_code,
+      timezone: city.timezone,
+      forecast_dates: city.data.map((d) => {
+          let weather_obj = {
+              date: d.valid_date,
+              max_temp: d.max_temp,
+              min_temp: d.min_temp,
+              temp_now: d.temp,
+              description: d.weather.description
+          };
+          return weather_obj;
+      })
+  }
+  return data
+  }
+}
+
 const app = express();
 app.use(cors());
 const port = process.env.port || 3000;
