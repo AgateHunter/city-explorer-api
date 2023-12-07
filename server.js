ea3685620133ff6907c0e6df142d11ef04e6ffd6
NATALIE STONE <NATALIE.I.STONE@OUTLOOK.COM>
2023-12-07 10:16:18 -0800

    added nodemon

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const weatherData = require('./data/weather.json');
+
+app.get('/', (req, res) => {
+  res.send('Hello, Express!');
+});
+
+app.get('/weather', (req, res) => {
+  res.send('Hello, Express!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
 
